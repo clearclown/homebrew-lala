d9180e6f0ecb5b8843b162639e7146f51b0cf708
clearClown(清楚系ピエロ) <clearcleaclown@gmail.com>
2026-01-20 19:52:11 +0900

    Update to v0.3.0

--- Formula/lala.rb
+++ Formula/lala.rb
@@ -1,8 +1,8 @@
 class Lala < Formula
   desc "Modern, lightweight multi-format text editor"
   homepage "https://github.com/clearclown/lala"
-  url "https://github.com/clearclown/lala/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3e0891cce56f4b884b14b1ad2242b3849f256957e0849d94d48de636e567cca0"
+  url "https://github.com/clearclown/lala/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "f76a898a5a2fa7281f6e69d5e5f8f3074415c675a9f09732e3bd7777658ef941"
   license "MIT OR Apache-2.0"
   head "https://github.com/clearclown/lala.git", branch: "main"
 
